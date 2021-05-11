1120e6ceb49bca28de8cc481bb6156b0193a030c
KlebanAndrii <smokymolla888@gmail.com>
2021-05-11 17:07:09 +0300

    lesson.3.5

---
+++ src/components/user/User.js
@@ -0,0 +1,11 @@
+export default function User({item, choseUser}) {
+    return (
+        <div>
+            <div>{item.username}</div>
+            <img src={'logo192.png'} alt=""/>
+            <div>
+                <button onClick={() => choseUser(item.id)}>details</button>
+            </div>
+        </div>
+    );
+}
