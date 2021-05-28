9bf5510c3e7cc983230042b4011d28c56e3c09f7
KlebanAndrii <smokymolla888@gmail.com>
2021-05-28 19:09:57 +0300

    redux.3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect} from "react";
+
+import {useDispatch, useSelector} from "react-redux";
+import {fetchProducts, addToWishlist, removeFromWishlist} from "./redux";
+
+
+const Header = () => {
+    const {wishlist} = useSelector(({products}) => products)
+
+    const wishlistTotalPrice = wishlist.reduce((acc, el) => {
+        return (acc += el.price);
+    }, 0)
+
+    return (
+        <>
+            <header style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center"
+            }}>
+                <h1>Our nice shop!</h1>
+
+
+                <div style={{
+                    display: "flex",
+                    alignItems: "center"
+                }}>
+                    <h1 style={{marginRight: "20px"}}>cart: {0}</h1>
+                    <h1 title={wishlistTotalPrice}>wishlist: {wishlist.length}</h1>
+                </div>
+            </header>
+            <hr/>
+        </>
+    )
+
+}
+
+const isInWishlist = (wishlist, id) => !!wishlist.find(el => el.id === id)
+
+const Products = () => {
+    const {wishlist} = useSelector(({products}) => products)
+
+    const {products, isProductsLoading} = useSelector(({products}) => products)
+    const dispatch = useDispatch()
+
+
+    useEffect(() => {
+        dispatch(fetchProducts({
+            // field: 'price',
+            // order: 'ASC'
+        }))
+    }, [])
+
+
+    if (isProductsLoading) {
+        return <div>LOADING!</div>
+    }
+
+
+    return (<div>
+        {products.map(product => (
+            <div key={product.id} style={{
+                width: "40%",
+                margin: "40px auto"
+            }}>
+                <button onClick={() => {
+                    isInWishlist(wishlist, product.id)
+                        ? dispatch(removeFromWishlist(product.id))
+                        : dispatch(addToWishlist(product.id))
+                }}>
+                    {isInWishlist(wishlist, product.id)
+                        ? 'remove from wishlist'
+                        : 'add to wishlist'}
+                </button>
+
+
+                <h4>{product.title}
+                    <br/>
+                    Price: {product.price}$</h4>
+                <p>{product.description}</p>
+                <img style={{
+                    width: "40%"
+                }} src={product.image} alt=""/>
+
+            </div>
+        ))}
+    </div>)
+
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <Products/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/redux/actionCreators/productsActionCreators.js
@@ -0,0 +1,40 @@
+import {
+    SET_PRODUCTS,
+    SET_PRODUCTS_LOADING,
+    RESET_PRODUCTS_LOADING,
+    ADD_TO_WISHLIST,
+    REMOVE_FROM_WISHLIST
+} from "../actionTypes";
+
+
+export const fetchProducts = (sortOptions) => async (dispatch) => {
+    try {
+        dispatch(setProductsLoading())
+        const resp = await fetch("https://fakestoreapi.com/products")
+        const data = await resp.json();
+
+
+        // data.sort((a, b) => {
+        //     if (sortOptions.order === 'ASC') {
+        //         return b[sortOptions.field] - a[sortOptions.field]
+        //     }
+        //     return a[sortOptions.field] - b[sortOptions.field]
+        // })
+
+
+        dispatch(setProducts(data))
+
+    } catch (e) {
+
+    } finally {
+        dispatch(resetProductsLoading())
+    }
+}
+
+
+export const setProducts = (payload) => ({type: SET_PRODUCTS, payload})
+export const setProductsLoading = () => ({type: SET_PRODUCTS_LOADING})
+export const resetProductsLoading = () => ({type: RESET_PRODUCTS_LOADING})
+
+export const addToWishlist = (payload) => ({type: ADD_TO_WISHLIST, payload})
+export const removeFromWishlist = (payload) => ({type: REMOVE_FROM_WISHLIST, payload})
